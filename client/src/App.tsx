d8512a9484881ab50fd9156e653c642a7447381a
bilwit <lolano07@gmail.com>
2024-08-27 17:21:20 -0700

    fixes responsive nav menu

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { AppShell, Burger, Group, MantineProvider, NavLink } from '@mantine/core';
+import { AppShell, Burger, Group, MantineProvider, NavLink, UnstyledButton } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import logo from './assets/logo_timearmor.webp';
 import Cameras from './Cameras';
@@ -14,21 +14,25 @@ function App() {
     <MantineProvider defaultColorScheme="auto">
       <AppShell
         header={{ height: 60 }}
-        navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
+        navbar={{ width: 300, breakpoint: 'sm', collapsed: { desktop: true, mobile: !opened } }}
         padding="md"
       >
         <AppShell.Header>
           <Group h="100%" px="md">
             <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-            <img src={logo} height={60} />
-            <b>Hackathon 2024</b>
-            <Group ml="xl" gap={0} visibleFrom="sm">
-            <NavLink 
-            label="IP Cameras" 
-          />
+            <Group justify="space-between" style={{ flex: 1 }}>
+              <img src={logo} height={60} />
+              <b>Hackathon 2024</b>
+              <Group ml="xl" gap={0} visibleFrom="sm">
+                <NavLink label="IP Cameras" />
+              </Group>
             </Group>
           </Group>
         </AppShell.Header>
+
+      <AppShell.Navbar py="md" px={4}>
+        <NavLink label="IP Cameras" />
+      </AppShell.Navbar>
 
         <AppShell.Main>
           <WsContext.Provider value={{ isConnected, socket }}>
